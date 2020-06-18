17a78c45e24201e87613cf87a7a720437b15b309
khullars16 <59499727+khullars16@users.noreply.github.com>
2020-06-18 12:00:26 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const btn = document.getElementById("btn");
 let input, listItem, text, editInput, val, valText;
 var count = 0;
-btn.addEventListener("click", () => {
+btn.addEventListener("click", function () {
+  fun();
+});
+document.addEventListener("keyup", function (e) {
+  if (e.keyCode == 13) {
+    fun();
+  }
+});
+
+function fun() {
   val = "init_" + count;
   valText = "init-" + count;
   input = document.getElementById("input").value;
   listItem = document.getElementById("list");
-  if(input == ""){
-      alert("add some task");
-  }
-  else{
+  if (input == "") {
+    alert("add some task");
+  } else {
     text = `<li style='list-style: none;'>
     <div class='row'>
     <div class='col-md-12 textField ext'>
-    <div id='${valText}'>
-    ${input}
-    </div>
+    <div id='${valText}'>${input}</div>
     <div class='thisinit'>
     <button id='${val}' onclick='edit(this.id)' class='btn btn-sm btn-primary delButton'><i class="icofont-ui-edit"></i></button>
     <button id='${val}' onclick='deletion(this.id)' class='btn btn-sm btn-primary delButton'><i class="icofont-ui-delete"></i></button>
@@ -23,31 +29,26 @@ btn.addEventListener("click", () => {
    </div>
   </div><hr>
    </li>`;
-  
-   listItem.insertAdjacentHTML("beforeend", text);
-   count++;
-   document.getElementById("input").value = null;
-     }   
-    });
+    listItem.insertAdjacentHTML("beforeend", text);
+    count++;
+    document.getElementById("input").value = null;
+  }
+}
 
-  var deletion = (element) => {
-      let x = document.getElementById(element);
-      let y = x.parentElement.parentElement.parentElement.parentElement;
-      let f = y.parentElement;
-      let del = f.removeChild(y);
-      console.log(f);
-    }
-        
-    var edit = (ele)=> {
-        let editInput = prompt("Enter Task to edit");
-        if(editInput == "" || editInput == null){
+var deletion = (element) => {
+  let x = document.getElementById(element);
+  let y = x.parentElement.parentElement.parentElement.parentElement;
+  let f = y.parentElement;
+  let del = f.removeChild(y);
+  console.log(f);
+};
 
-        }
-        else{
-        let testing = document.getElementById(ele);
-        let parentEle = testing.parentElement.previousElementSibling;
-        parentEle.innerHTML = editInput; 
-        }
-    }
-
-
+var edit = (ele) => {
+  let editInput = prompt("Enter Task to edit");
+  if (editInput == "" || editInput == null) {
+  } else {
+    let testing = document.getElementById(ele);
+    let parentEle = testing.parentElement.previousElementSibling;
+    parentEle.innerHTML = editInput;
+  }
+};
